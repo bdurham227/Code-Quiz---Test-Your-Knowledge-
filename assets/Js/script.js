9493a9e4fd613839ac5c1299da092c61a6c47597
bdurham227 <bdurham227@gmail.com>
2021-03-02 16:49:53 -0500

    adding html/css changes and js

---
+++ assets/Js/script.js
@@ -0,0 +1,37 @@
+// set variables for quiz
+var highscores;
+var time;
+var correct;
+var incorrect;
+var selectAnswerArray = [];
+
+
+
+//set variables to reference the DOM elements
+var quizContainer = document.getElementById("quiz");
+var resultsContainer = document.getElementById("results");
+var submitButton = document.getElementById("submit");
+var startButton = document.getElementById("start-btn");
+var timeEl = document.querySelector("#timer");
+var highScoresBtn = document.querySelector("#high-btn");
+var highscoresEl = document.querySelector("#highscores");
+
+
+//create function to build the quiz
+
+//set timerinterval
+
+//show starting time
+
+//create function to get questions
+
+//loop over choices
+
+//create display buttons for choice
+
+//display on the page
+
+
+//create function to end the game
+
+//create function

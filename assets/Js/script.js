613a4fd65369ad9c5df8ae8cf9b6b79fee79e5ee
bdurham227 <bdurham227@gmail.com>
2021-03-03 18:29:44 -0500

    created variables for quiz and writing timer function with event listener

--- assets/Js/script.js
+++ assets/Js/script.js
@@ -27,19 +27,82 @@ var questions = [
     },
 
 ];
-//
+//variables holding quiz scores/current question
+var score = 0;
+var currentQuestion = 0;
 
+//variables for DOM
 var quizContainer = document.querySelector("#quiz-container");
-//var startBtn = document.querySelector("#start");
+//where questions will be displayed
+var questionsEl = document.querySelector("#questions-el");
+//create a new ul for the html here
+var quizUL = document.createElement("ul");
+var startBtn = document.querySelector("start-btn");
 
 
 
 //timer variables
 var timerLeft = 60;
-var timeEl = document.querySelector("#timer");
+var timeEl = document.querySelector("#timer-el");
 var penalty = 10;
 var timerInterval = 0;
 
 
+startBtn.addEventListener("click", function () {
+    if (timerInterval === 0) {
+        timeEl.textContent = timerLeft + "second remaining";
+        timerLeft--;
+    if (timerInterval <= 0){
+        clearInterval(timerInterval);
+        finished(); //still needs to be defined
+    }
+    } 
+})
 
 
+//we need an event listener on a button to start the quiz--probably need an anonymous function inside
+//document.addEventListener("click", startQuiz);
+
+
+//we need a start game function + need a timer function with setInterval and clearInterval
+
+
+//var timerInterval = setInterval(function () {
+   // if (timerLeft > 1) {
+      //  timeEl.textContent = timerLeft + " seconds remaining";
+        //console.log(timerLeft);
+   // } //else if ()
+    //clearInterval(timerInterval);
+//})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//need a function to build the quiz with if/else statements most likely
+
+// need methods to display the quiz onto the DOM most likely using append/append child
+//textcontent and innerhtml
+
+
+
+
+
+
+
+
+
+
+//startQuiz();
